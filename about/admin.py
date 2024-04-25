74e5ae8a7fd7676b77c444c8bc0b718a9932e2cd
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-04-25 07:18:37 +0000

    Register About model in admin.py.

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import About
+
+
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    summoernote_fields = ('content',)
 
 # Register your models here.
+
--- about/models.py
+++ about/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class About(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now_add=True)
